0663bc1ab02ffbb63627a3d4909b9f754132bb00
CraZzyIceeNicK <405965@vistacollege.nl>
2021-04-06 14:14:13 +0200

    Update contact.php
    contact page title "Buurtzorg" ervoor gezet

--- contact.php
+++ contact.php
@@ -7,7 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style.css" type="text/css">
   <link rel="stylesheet" href="css/contact.css" type="text/css">
-  <title>VistaCare - Contact</title>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  <title>Buurtzorg VistaCare - Contact</title>
 </head>
 
 <body>
