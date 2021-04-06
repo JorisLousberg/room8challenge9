48d5ca35a1065cc9747416b3727487f22fa567dd
RoyTeheux <75972921+RoyTeheux@users.noreply.github.com>
2021-04-06 11:15:31 +0200

    homepagina

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+        <title>Buurtzorg VistaCare - Home</title>
+</head>
+
+<body>
+    <div class="pagecon">  
+        <nav>
+        <section class="navContainer">
+
+        <div class="logoWrapper"> <a href="index.php"><img src="images/logo_buurtzorg.png" alt="logo website" style="width:15.1vh;height:11vh;"></a> </div>
+            <nav class="navMenu">
+                <ul class="navList">
+                    <li class="navItem"><a class="navLink" href="index.php">Homepagina</a></li>     
+                </ul>
+                <ul class="navList">
+                    <li class="navItem"><a class="navLink" href="#">Werken bij</a></li>     
+                </ul>
+                <ul class="navList">
+                    <li class="navItem"><a class="navLink" href="#">Contact Opnemen</a></li>     
+                </ul>
+            </nav>
+                </section> 
+                </nav>
+
+    <div class="mainbody">
+        <div class="bannerimage"><img src="images/bannerimage.png" alt="banner homepage" style="width:165vh;height:55vh;padding-left:45vh;"></div>
+
+        <section id="textbox1">
+                <section class="textbox1_text">Lorem ipsum.</section>
+            </section>
+
+        <section id="textbox2">
+                <section class="textbox2_text">Lorem ipsum.</section>
+            </section>
+        </div>
+
+    <footer>
+        <div class="footer">
+              <div id="footercopyright">&copy; VistaCare Buurtzorg</div>
+              <div id="footertext2">telefoonnummer: 06 87654321</div>
+        </div>
+    </footer> 
+</body>   
