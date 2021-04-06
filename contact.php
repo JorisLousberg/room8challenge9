dedd31dac94dbf831d684abe81066e17d6d0a851
CraZzyIceeNicK <405965@vistacollege.nl>
2021-04-06 10:23:43 +0200

    contact page

---
+++ contact.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/mainstyle.css" type="text/css">
+  <link rel="stylesheet" href="css/contact.css" type="text/css">
+  <title>VistaCare - Contact</title>
+</head>
+<body>
+
+  <div class="navbar">
+    Home, Contact, Nieuws, Login
+  </div>
+
+  <div class="contactbody">
+
+    <div class="inhoudcontact">
+
+      <div id="AlgemeneInfo">
+        <h2>Algemene Info</h2>
+        <div id="tekst">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+          sapiente officiis modi at sunt excepturi expedita sint?
+        </div>
+      </div>
+
+      <div id="CoronaInfo">
+        <h2>Corona Info</h2>
+        <div id="tekst">
+          In verband met Corona kunt u ons hoofdkantoor alleen op telefonische afspraak bezoeken. </br>
+          Bij uw bezoek dient u uiteraard een mondkapje te dragen. </br>
+          Wij voorzien u bij de ingang van handgel en tissues. </br>
+        </div>
+      </div>
+
+      <div id="AdresInfo">
+        <h2>Adres Info</h2>
+        <div id="HoofdkantoorTekst">
+          <b>Hoofdkantoor:</b> </br>
+          Twentelaan 15, 7609 RE
+        </div>
+        <div id="LocatieTekst">
+          <b>Maastricht:</b> Sibemaweg 20, 6224 DC </br>
+          <b>Sittard:</b> Arendstraat 12, 6135 KT </br>
+          <b>Heerlen:</b> Valkenburgerweg 148, 6419 AW </br>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="footer">
+    Copyright C 2021. Telefoonnummer *************
+  </div>
+
+</body>
+</html>
